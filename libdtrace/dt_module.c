1fac1e479b3d7b1f22c63c1699e7a186cfe28b28
Kris Van Hees <kris.van.hees@oracle.com>
2026-06-17 14:59:33 +0000

    Fix typo in conditional
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

--- libdtrace/dt_module.c
+++ libdtrace/dt_module.c
@@ -200,7 +200,7 @@ dt_module_init_elf(dtrace_hdl_t *dtp, dt_module_t *dmp)
 	int fd, err, bits;
 	size_t shstrs;
 
-	if (!dmp->dm_file[0] == '\0') {
+	if (dmp->dm_file[0] == '\0') {
 		dt_dprintf("failed to open ELF file for module %s: "
 		    "no file name known\n", dmp->dm_name);
 		return dt_set_errno(dtp, EDT_NOTLOADED);
